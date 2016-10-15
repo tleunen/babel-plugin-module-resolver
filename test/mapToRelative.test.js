2bb9a9553006f7409c0ede3f20908134f6ca84a3
Tommy Leunen <tommy.leunen+github@gmail.com>
2016-10-15 12:22:12 -0400

    chore(tests): Move to jest

--- test/mapToRelative.test.js
+++ test/mapToRelative.test.js
@@ -1,6 +1,5 @@
-/* eslint-env mocha */
+/* eslint-env jest */
 import path from 'path';
-import assert from 'assert';
 import mapToRelative from '../src/mapToRelative';
 
 describe('mapToRelative', () => {
@@ -9,14 +8,14 @@ describe('mapToRelative', () => {
             const currentFile = './utils/test/file.js';
             const result = mapToRelative(path.resolve('./test'), currentFile, 'utils/dep');
 
-            assert.strictEqual(result, '../dep');
+            expect(result).toBe('../dep');
         });
 
         it('with absolute filename', () => {
             const currentFile = path.join(process.cwd(), './utils/test/file.js');
             const result = mapToRelative(process.cwd(), currentFile, 'utils/dep');
 
-            assert.strictEqual(result, '../dep');
+            expect(result).toBe('../dep');
         });
     });
 });
