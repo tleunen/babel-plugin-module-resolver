caeaf0f0c9271acabb659ba1f2d7b9aea4d2129c
fatfisz <fatfisz@gmail.com>
2017-04-08 20:42:54 +0200

    Update the tests

--- test/mapToRelative.test.js
+++ test/mapToRelative.test.js
@@ -1,16 +1,18 @@
 import path from 'path';
+
 import mapToRelative from '../src/mapToRelative';
+
 
 describe('mapToRelative', () => {
   describe('should map to relative path with a custom cwd', () => {
-    it('with relative filename', () => {
+    it('with a relative filename', () => {
       const currentFile = './utils/test/file.js';
       const result = mapToRelative(path.resolve('./test'), currentFile, 'utils/dep');
 
       expect(result).toBe('../dep');
     });
 
-    it('with absolute filename', () => {
+    it('with an absolute filename', () => {
       const currentFile = path.join(process.cwd(), './utils/test/file.js');
       const result = mapToRelative(process.cwd(), currentFile, 'utils/dep');
 
