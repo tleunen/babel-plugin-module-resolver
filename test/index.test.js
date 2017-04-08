3aff28043ec707d1fed37e6e5eb4897ca50a5acd
Nicholas Rakoto <nico@nrako.com>
2017-04-08 22:19:13 +0200

    Typo

--- test/index.test.js
+++ test/index.test.js
@@ -196,7 +196,7 @@ describe('module-resolver', () => {
         );
       });
 
-      describe('should not resolve a path outisde of the root directory', () => {
+      describe('should not resolve a path outside of the root directory', () => {
         testRequireImport(
           'lodash/omit',
           'lodash/omit',
