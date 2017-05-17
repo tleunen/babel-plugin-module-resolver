44faf2526dbe0b0c94c3dbd2d4b4ad3f00709891
fatfisz <fatfisz@gmail.com>
2017-05-17 22:29:28 +0200

    Move the transformed methods into options

--- src/transformers/call.js
+++ src/transformers/call.js
@@ -5,20 +5,11 @@ import {
 } from '../utils';
 
 
-const patterns = [
-  'require',
-  'require.resolve',
-  'System.import',
-  'jest.genMockFromModule',
-  'jest.mock',
-  'jest.unmock',
-  'jest.doMock',
-  'jest.dontMock',
-];
-
 export default function transformCall(nodePath, state) {
   const calleePath = nodePath.get('callee');
-  const isNormalCall = patterns.some(pattern => matchesPattern(state.types, calleePath, pattern));
+  const isNormalCall = state.opts.transformedMethods.some(
+    pattern => matchesPattern(state.types, calleePath, pattern),
+  );
 
   if (isNormalCall || isImportCall(state.types, nodePath)) {
     mapPathString(nodePath.get('arguments.0'), state);
