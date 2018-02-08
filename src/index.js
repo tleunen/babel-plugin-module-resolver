12a2d07c215e427698179eb6ef29cb5a7f31b5ce
Daniel Tschinder <daniel@tschinder.de>
2018-02-08 19:06:29 +0100

    feat: run plugin also on Program exit to handle dynamically added imports from other transforms (#269)

--- src/index.js
+++ src/index.js
@@ -18,6 +18,9 @@ const visitor = {
     enter(programPath, state) {
       programPath.traverse(importVisitors, state);
     },
+    exit(programPath, state) {
+      programPath.traverse(importVisitors, state);
+    },
   },
 };
 
@@ -29,7 +32,14 @@ export default ({ types }) => ({
 
     const currentFile = file.opts.filename;
     this.normalizedOpts = normalizeOptions(currentFile, this.opts);
+    // We need to keep track of all handled nodes so we do not try to transform them twice,
+    // because we run before (enter) and after (exit) all nodes are handled
+    this.moduleResolverVisited = new Set();
   },
 
   visitor,
+
+  post() {
+    this.moduleResolverVisited.clear();
+  },
 });
--- src/transformers/call.js
+++ src/transformers/call.js
@@ -6,12 +6,17 @@ import {
 
 
 export default function transformCall(nodePath, state) {
+  if (state.moduleResolverVisited.has(nodePath)) {
+    return;
+  }
+
   const calleePath = nodePath.get('callee');
   const isNormalCall = state.normalizedOpts.transformFunctions.some(
     pattern => matchesPattern(state.types, calleePath, pattern),
   );
 
   if (isNormalCall || isImportCall(state.types, nodePath)) {
+    state.moduleResolverVisited.add(nodePath);
     mapPathString(nodePath.get('arguments.0'), state);
   }
 }
